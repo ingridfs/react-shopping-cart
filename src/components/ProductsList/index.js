62e442c44b860fbd014881322095b21dd10ac0af
ingridfs <ingrid.fernandes.santos@hotmail.com>
2021-03-04 22:07:13 -0300

    Product List component created

---
+++ src/components/ProductsList/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './styles.css';
+
+export default function ProductsList() {
+
+
+  return (
+    <div className="product-list">
+      <div className="product-list-item">product name 1</div>
+      <div className="product-list-item">product name 2</div>
+      <div className="product-list-item">product name 3</div>
+      <div className="product-list-item">product name 4</div>
+      <div className="product-list-item">product name 5</div>
+    </div>
+  );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import ProductsList from '../../components/ProductsList';
+
 import './styles.css';
 
 export default function Home() {
   return (
     <div className="container">
-      Home page - Products List
+      <h1 className="page-title">Home page</h1>
+      <ProductsList />
     </div>
   );
 }
